0e26ee86d4e282a61063180f21c27a4af9ac149b
Brett Jones <blockloop@users.noreply.github.com>
2018-03-26 19:44:46 -0500

    allow query strings

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const Chance = require("chance");
+const autoParse = require("auto-parse");
 
 const app = express();
 const chance = new Chance();
 const PORT = 3000;
 
-app.get("/:kind", (req, res) => {
-	const kind = req.params.kind || "string";
-	const fn = chance[kind];
-	if (typeof fn === "function") {
-		const str = `${fn.call(chance)}`;
-		res.status(200).end(str);
-		return;
-	}
-	res.status(404).end("not found");
-});
+app.get("/:kind", handle);
+app.get("/", handle);
 
 app.listen(PORT, () => console.log(`listening at :${PORT}`));
+
+function handle(req, res) {
+	const kind = req.params.kind || "string";
+	const fn = chance[kind];
+	if (typeof fn !== "function") {
+		res.status(404).end("not found");
+		return;
+	}
+
+	const q = autoParse(req.query);
+	const val = Reflect.apply(fn, chance, [q]);
+	res.status(200).end(`${val}`);
+}
+
