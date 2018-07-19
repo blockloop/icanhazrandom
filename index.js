eb291517e2c66aa1c5fd8cf9e5976ab7b5441cfb
Brett Jones <blockloop@users.noreply.github.com>
2018-07-19 13:17:57 -0500

    add /help

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const autoParse = require("auto-parse");
 const app = express();
 const PORT = 3000;
 
+app.get("/help", help);
 app.get("/:kind", handle);
 app.get("/", handle);
 
 app.listen(PORT, () => console.log(`listening at :${PORT}`));
+
+function help(req, res) {
+        let keys = []
+        for (var prop in chance) {
+                keys.push(prop)
+        }
+        keys = keys.
+                filter((k) => (typeof chance[k] === "function")).
+                map((k) => `    /${k}`).
+                join("\n")
+        res.status(200).end(`<pre>available options are:\n${keys}\n</pre>`);
+}
 
 function handle(req, res) {
 	const kind = req.params.kind || "string";
